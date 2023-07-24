65d93e79d45a09609e53964bc86fc10ad0a86d47
GabrieleGrossi <125304063+GabrieleGrossi@users.noreply.github.com>
2023-07-24 12:40:39 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('title', 'Welcome')
+@section('main-content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>
+                Welcome
+            </h1>
+            <h3>
+                Ciao
+            </h3>
+        </div>
+    </div>
+</div>
+@endsection
